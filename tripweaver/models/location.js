280ba9c54152a4bd177e6cdb76b5425947d29fca
XSectorZ <panatschool@gmail.com>
2024-09-02 17:28:50 +0700

    Chang some attr

--- tripweaver/models/location.js
+++ tripweaver/models/location.js
@@ -5,9 +5,9 @@ const locationSchema = new Schema({
     province: { type: String, required: true },
     district: { type: String, required: true },
     subDistrict: { type: String, required: true },
-    ISO_3166_code: { type: Number, required: true },
-    zip_code: { type: Number, required: true },
-    geo_code: { type: Number, required: true },
+    province_code: { type: Number, required: true },
+    district_code: { type: Number, required: true },
+    sub_district_code: { type: Number, required: true },
 }, { _id: false });
 
 export default locationSchema;
