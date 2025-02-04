1838953881fcd5962e3f7592264daaddebc721c1
kaooatprm <kaooat.prm@gmail.com>
2025-02-04 22:50:48 +0700

    Fix API get method to not cache data

--- tripweaver/src/app/api/accommodation/getAll/route.ts
+++ tripweaver/src/app/api/accommodation/getAll/route.ts
@@ -1,3 +1,4 @@
+export const revalidate = 0;
 import { NextResponse } from "next/server";
 import { connectMongoDB } from '../../../../../lib/mongodb'
 import Accommodation from "../../../../../models/accommodation";
